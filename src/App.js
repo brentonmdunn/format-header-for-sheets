408c0ddbe82fe7eea99d7071f2a469462252727c
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-04-02 18:38:43 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useState } from 'react';
 
+// Function to format header
+function formatForSheet(header) {
+  const namePattern = /Name: (.+?)\n/;
+  const nameMatch = header.match(namePattern);
+  const emailPattern = /Email: (.+?)\n/;
+  const emailMatch = header.match(emailPattern);
+
+  if (nameMatch && emailMatch) {
+    return { name: nameMatch[1], email: emailMatch[1] };
+  } else {
+    return { name: '', email: '' };
+  }
+}
+
+// Component for the main app
 function App() {
+  const [header, setHeader] = useState('');
+  const [output, setOutput] = useState({ name: '', email: '' });
+
+  const [copySuccess, setCopySuccess] = useState('');
+  const textAreaRef = useRef(null);
+
+  function copyToClipboard(e) {
+
+    if (output.name && output.email) {
+      const data = `${output.name}\t${output.email}`;
+      navigator.clipboard.writeText(data)
+        .then(() => {
+          setCopySuccess('Copied!');
+        })
+        .catch((error) => {
+          console.error('Failed to copy:', error);
+          setCopySuccess('Failed to copy');
+        });
+    } 
+  };
+
+  const handleInputChange = (event) => {
+    setHeader(event.target.value);
+    setOutput(formatForSheet(event.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <textarea ref={textAreaRef} value={header} onChange={handleInputChange}></textarea>
+      <button onClick={copyToClipboard}>Copy</button>
+      {copySuccess}
+      <div className='results'>
+        <p>Name: {output.name}</p>
+        <p>Email: {output.email}</p>
+      </div>
     </div>
   );
 }
