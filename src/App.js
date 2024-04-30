02ba6b03bf3212b9309048f97e750671b41a5d1e
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-04-30 00:07:42 -0700

    Add PID field

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ function formatForSheet(header) {
   const nameMatch = header.match(namePattern);
   const emailPattern = /Email: (.+?)\n/;
   const emailMatch = header.match(emailPattern);
+  const pidPattern = /PID: ([AU]\d{8})\n/;
+  const pidMatch = header.match(pidPattern);
 
-  if (nameMatch && emailMatch) {
-    return { name: nameMatch[1], email: emailMatch[1] };
+  if (nameMatch && emailMatch && pidMatch) {
+    return { name: nameMatch[1], email: emailMatch[1] , pid: pidMatch[1]};
   } else {
-    return { name: '', email: '' };
+    return { name: '', email: '', pid: '' };
   }
 }
 
 // Component for the main app
 function App() {
   const [header, setHeader] = useState('');
-  const [output, setOutput] = useState({ name: '', email: '' });
+  const [output, setOutput] = useState({ name: '', email: '', pid: '' });
 
   const [copySuccess, setCopySuccess] = useState('');
   const textAreaRef = useRef(null);
@@ -26,7 +28,7 @@ function App() {
   function copyToClipboard(e) {
 
     if (output.name && output.email) {
-      const data = `${output.name}\t${output.email}`;
+      const data = `${output.name}\t${output.email}\t${output.pid}`;
       navigator.clipboard.writeText(data)
         .then(() => {
           setCopySuccess('Copied!');
@@ -41,6 +43,7 @@ function App() {
   const handleInputChange = (event) => {
     setHeader(event.target.value);
     setOutput(formatForSheet(event.target.value));
+    setCopySuccess('')
   };
 
   return (
@@ -51,6 +54,7 @@ function App() {
       <div className='results'>
         <p>Name: {output.name}</p>
         <p>Email: {output.email}</p>
+        <p>PID: {output.pid}</p>
       </div>
     </div>
   );
